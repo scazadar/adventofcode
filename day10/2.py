11c15c029f76800a2e11dd5b5b9dc3dc956ed31f
scazadar <mb.gielen@gmail.com>
2021-12-10 07:51:23 +0100

    day10

--- day10/2.py
+++ day10/2.py
@@ -9,18 +9,15 @@ scores = []
 for line in f:
     score = 0
     next_closing_chars = []
-    error = False
     for _ in range(len(line)):
         if(line[_] in OPENINGCHARS):
             next_closing_chars.append(CLOSINGCHARS[OPENINGCHARS.index(line[_])])
         elif(next_closing_chars[-1] != line[_]):
             score += ERROR_SCORES.get(line[_])
-            error = True
             break
         else:
             next_closing_chars = next_closing_chars[:-1]
-    if(not error): 
-        error = False
+    else: 
         for _ in reversed(next_closing_chars):
             score = score * 5 + ERROR_SCORES.get(_)
         scores.append(score)
