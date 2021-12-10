684bec54eb4db182dbba8c32de35cfbeb856f98a
scazadar <mb.gielen@gmail.com>
2021-12-10 07:58:12 +0100

    day10

--- day10/day10.py
+++ day10/day10.py
@@ -3,23 +3,27 @@ f = [_ for _ in [x.strip() for x in open("10.txt").readlines()]]
 
 OPENINGCHARS = "([{<"
 CLOSINGCHARS = ")]}>"
-ERROR_SCORES = {")":1,"]":2,"}":3,">":4}
-
+ERROR_SCORES_1 = {")":3,"]":57,"}":1197,">":25137}
+ERROR_SCORES_2 = {")":1,"]":2,"}":3,">":4}
 scores = []
+score_1 = 0
+
 for line in f:
-    score = 0
+    score_2 = 0
     next_closing_chars = []
     for _ in range(len(line)):
         if(line[_] in OPENINGCHARS):
             next_closing_chars.append(CLOSINGCHARS[OPENINGCHARS.index(line[_])])
         elif(next_closing_chars[-1] != line[_]):
+            score_1 += ERROR_SCORES_1.get(line[_])
             break
         else:
             next_closing_chars = next_closing_chars[:-1]
     else: 
         for _ in reversed(next_closing_chars):
-            score = score * 5 + ERROR_SCORES.get(_)
-        scores.append(score)
+            score_2 = score_2 * 5 + ERROR_SCORES_2.get(_)
+        scores.append(score_2)
 
 scores.sort()
-print(scores[int(len(scores)/2)])
+print(f"Part1: {score_1}")
+print(f"Part2: {scores[int(len(scores)/2)]}")
