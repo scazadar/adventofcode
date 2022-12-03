79d6d836565bd016e889986e39ae1724cf4576d3
scazadar <mb.gielen@gmail.com>
2022-12-03 12:58:13 +0100

    day3

--- 2022/day2/1&2.py
+++ 2022/day2/1&2.py
@@ -6,8 +6,9 @@ import time
 start = time.time()
 
 #Main
-print("Part1: ",sum([["B X", "C Y", "A Z", "A X", "B Y", "C Z", "C X", "A Y", "B Z"].index(_.strip())+1 for _ in open("inputs/day2").readlines()]))
-print("Part2: ",sum([["B X", "C X", "A X", "A Y", "B Y", "C Y", "C Z", "A Z", "B Z"].index(_.strip())+1 for _ in open("inputs/day2").readlines()]))
+f = open("inputs/day2").readlines()
+print("Part1: ",sum([["B X", "C Y", "A Z", "A X", "B Y", "C Z", "C X", "A Y", "B Z"].index(_.strip())+1 for _ in f]))
+print("Part2: ",sum([["B X", "C X", "A X", "A Y", "B Y", "C Y", "C Z", "A Z", "B Z"].index(_.strip())+1 for _ in f]))
 
 #Zeit Ende
 ende = time.time()
---
+++ 2022/day3/1&2.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+#Imports
+import time
+
+#Zeit Start
+start = time.time()
+
+#Main
+f = [_.strip() for _ in open("inputs/day3").readlines()]
+print("Part1: ", sum([_-96 if _ > 96 else _-38 for _ in [ord(list(set(list(_)[:int(len(_)/2)]).intersection(set(list(_)[int(len(_)/2):])))[0]) for _ in f]]))
+print("Part2: ", sum([_-96 if _ > 96 else _-38 for _ in [ord(list([set(_) for _ in f][_].intersection([set(_) for _ in f][_+1]).intersection([set(_) for _ in f][_+2]))[0]) for _ in range(0,len(f),3)]]))
+
+#Zeit Ende
+ende = time.time()
+print('Zeit:   {:5.3f}s'.format(ende-start))
