ddee7916d7e35fc9d5b0b18a9f897d8419be13ee
scazadar <mb.gielen@gmail.com>
2022-12-03 13:03:52 +0100

    schneller

--- 2022/day3/1&2.py
+++ 2022/day3/1&2.py
@@ -8,7 +8,8 @@ start = time.time()
 #Main
 f = [_.strip() for _ in open("inputs/day3").readlines()]
 print("Part1: ", sum([_-96 if _ > 96 else _-38 for _ in [ord(list(set(list(_)[:int(len(_)/2)]).intersection(set(list(_)[int(len(_)/2):])))[0]) for _ in f]]))
-print("Part2: ", sum([_-96 if _ > 96 else _-38 for _ in [ord(list([set(_) for _ in f][_].intersection([set(_) for _ in f][_+1]).intersection([set(_) for _ in f][_+2]))[0]) for _ in range(0,len(f),3)]]))
+fset = [set(_) for _ in f]
+print("Part2: ", sum([_-96 if _ > 96 else _-38 for _ in [ord(list(fset[_].intersection(fset[_+1]).intersection(fset[_+2]))[0]) for _ in range(0,len(f),3)]]))
 
 #Zeit Ende
 ende = time.time()
