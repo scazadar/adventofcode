84b11fae0b15d2a53a88640bfaf0cad26ad5a14d
scazadar <mb.gielen@gmail.com>
2022-12-04 12:22:02 +0100

    day4

--- 2022/day4/1&2.py
+++ 2022/day4/1&2.py
@@ -6,9 +6,9 @@ import time
 start = time.time()
 
 #Main
-f = open("inputs/day4").readlines()
-print(sum([1 if _[0].issubset(_[1]) or _[1].issubset(_[0]) else 0 for _ in[[set(list(range(int(_.split("-")[0]),int(_.split("-")[1])+1))) for _ in _.strip().split(",")] for _ in f]]))
-print(sum([1 if len(_[0].intersection(_[1])) > 0 else 0 for _ in[[set(list(range(int(_.split("-")[0]),int(_.split("-")[1])+1))) for _ in _.strip().split(",")] for _ in f]]))
+f = [[set(list(range(int(_.split("-")[0]),int(_.split("-")[1])+1))) for _ in _.strip().split(",")] for _ in open("inputs/day4").readlines()]
+print("Part1: ", sum([1 if _[0].issubset(_[1]) or _[1].issubset(_[0]) else 0 for _ in f]))
+print("Part2: ", sum([1 if len(_[0].intersection(_[1])) > 0 else 0 for _ in f]))
 
 #Zeit Ende
 ende = time.time()
