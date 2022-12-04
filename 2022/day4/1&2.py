0f000c48ad8c34f2b96a49ae22d7ba6bf7be326a
scazadar <mb.gielen@gmail.com>
2022-12-04 18:04:55 +0100

    kürzer

--- 2022/day4/1&2.py
+++ 2022/day4/1&2.py
@@ -6,7 +6,7 @@ import time
 start = time.time()
 
 #Main
-f = [[set(list(range(int(_.split("-")[0]),int(_.split("-")[1])+1))) for _ in _.strip().split(",")] for _ in open("inputs/day4").readlines()]
+f = [[set(range(int(_.split("-")[0]),int(_.split("-")[1])+1)) for _ in _.strip().split(",")] for _ in open("inputs/day4").readlines()]
 print("Part1: ", sum([1 if _[0].issubset(_[1]) or _[1].issubset(_[0]) else 0 for _ in f]))
 print("Part2: ", sum([1 if len(_[0].intersection(_[1])) > 0 else 0 for _ in f]))
 
