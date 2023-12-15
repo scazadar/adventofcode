a0e90e12c09442d29bbd5652be3a695f4e969ffa
scazadar <mb.gielen@gmail.com>
2023-12-15 23:56:45 +0100

    day13

--- 2023/day13.py
+++ 2023/day13.py
@@ -6,7 +6,7 @@ import time
 start = time.time()
 
 #Main
-file = [_ for _ in open("2023/inputs/day13.sample").read().split("\n")]
+file = [_ for _ in open("2023/inputs/day13").read().split("\n")]
 file.append("")
 
 patterns = []
@@ -20,8 +20,8 @@ for _ in file:
 
 hMirrors = []
 vMirrors = []
-#althMirrors = []
-#altvMirrors = []
+althMirrors = []
+altvMirrors = []
 
 def findMirror(i,p):
     if(len(p[:i]) < len(p[i:])):
@@ -31,13 +31,26 @@ def findMirror(i,p):
         u = p[i-len(p[i:]):i]
         d = p[i:]
     d.reverse()
-    return u == d
+    matches = 0
+    mismatches = 0
+    for x,_ in enumerate(u):
+        if(_ == d[x]):
+            matches += 1
+        
+        for i,s in enumerate(_):
+            if(s != d[x][i]):
+                mismatches += 1
+        
+    return [len(u),matches,mismatches]
         
 for p in patterns:
     for y in range(1,len(p)):
         lines = len(p[:y])
-        if(findMirror(y,p)):
+        m = findMirror(y,p)
+        if(m[0]==m[1]):
             hMirrors.append(lines)
+        if(m[0]==m[1]+1 and m[2] == 1):
+            althMirrors.append(lines)
   
     hRows = []
     for x,_ in enumerate(p[0]):
@@ -46,14 +59,17 @@ for p in patterns:
     
     for x in range(1,len(hRows)):
         lines = len(hRows[:x])  
-        if(findMirror(x,hRows)):
+        m = findMirror(x,hRows)
+        if(m[0]==m[1]):
             vMirrors.append(lines)
+        if(m[0]==m[1]+1 and m[2] == 1):
+            altvMirrors.append(lines)
 
         
 
     
 print(f"Part1: {sum(vMirrors) + (sum(hMirrors)*100)}")
-#print(f"Part2: {sum(altvMirrors) + (sum(althMirrors)*100)}")
+print(f"Part2: {sum(altvMirrors) + (sum(althMirrors)*100)}")
 #Zeit Ende
 ende = time.time()
 print('Zeit:   {:.3f}s'.format(ende-start))
