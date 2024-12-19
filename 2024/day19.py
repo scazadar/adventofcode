27303c64e6baeeb5c5d9c9fbd42a53519ac5f739
scazadar <mb.gielen@gmail.com>
2024-12-19 20:48:07 +0000

    day19

--- 2024/day19.py
+++ 2024/day19.py
@@ -5,18 +5,66 @@ import time
 # Zeit Start
 startZeit = time.time()
 
-file = "2024/inputs/day19.sample"
+file = "2024/inputs/day19"
 
 towels,designs= open(file).read().split("\n\n")
 
-towels = [_.strip() for _ in towels.split(",")]
-designs = designs.split("\n")
+towels = set([_.strip() for _ in towels.split(",")])
+designs = set(designs.split("\n"))
 
-print(towels)
-print(designs)
+def getNextStrings(current,design):
+    new = []
+    if not current:
+        for towel in towels:
+            if(design.startswith(towel)):
+                new.append(towel)
+    else:
+        for c in current:
+            for towel in towels:
+                if(design.replace(c,"",1).startswith(towel) and len(c+towel) <= len(design) and c+towel not in new):
+                    new.append(c + towel)        
+    return new
 
-   
-        
+c = 0
+for design in designs:
+    current = []
+    while True:
+        current = getNextStrings(current,design)
+        if(len(current) == 0):
+            break
+        elif(design in current):
+            c += 1
+            break
+
+print(f"Part1: {c}")
+
+
+#part2
+def getNextStrings2(current,design):
+    new = []
+    if not current:
+        for towel in towels:
+            if(design.startswith(towel)):
+                new.append([towel])
+    else:
+        for c in current:
+            for towel in towels:
+                if(design.replace("".join(c),"",1).startswith(towel) and len(c+[towel]) <= len(design) and c+[towel] not in new):
+                    new.append(c + [towel])        
+    return new
+
+c = 0
+possible = []
+for design in designs:
+    current = []
+    while True:
+        current = getNextStrings2(current,design)
+        if(len(current) == 0):
+            break
+        elif(design in ["".join(_) for _ in current]):
+            c += ["".join(_) for _ in current].count(design)
+
+print(f"Part2: {c}")
         
 
 # Zeit Ende
