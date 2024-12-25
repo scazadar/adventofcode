279b8d752fb08ee2f82927697f12ba0988c93273
scazadar <mb.gielen@gmail.com>
2024-12-25 21:58:19 +0100

    day25

---
+++ 2024/day25.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+# Imports
+import time
+
+# Zeit Start
+startZeit = time.time()
+
+file = "2024/inputs/day25"
+
+locksAndKeys = [_.split("\n") for _ in open(file).read().split("\n\n")]
+locks = [[_.index('.') -1 for _ in zip(*[tuple(_ for _ in _) for _ in _])] for _ in locksAndKeys if _[0] == '#####']
+keys = [[len(_) - _.index('#') -1 for _ in zip(*[tuple(_ for _ in _) for _ in _])] for _ in locksAndKeys if _[0] != '#####']
+
+fit = 0
+for lock in locks:
+    for key in keys:
+        if(not any(x >= 6 for x in [l+k for l,k in zip(lock,key)])):
+            fit += 1
+            
+print(f"Part1: {fit}")
+            
+
+# Zeit Ende
+endeZeit = time.time()
+print('Zeit:   {:.3f}s'.format(endeZeit-startZeit))
+
