9ec1ae0e7774e658aa5c1c4ce23bd1f579605ad7
scazadar <mb.gielen@gmail.com>
2025-12-02 08:54:21 +0100

    day2

--- 2025/day2.py
+++ 2025/day2.py
@@ -9,23 +9,18 @@ start = time.time()
 ranges = [[tuple([int(r[0]),int(r[1])]) for r in re.findall(r"(\d+)-(\d+)",s)] for s in open("inputs/day2").read().split("\n")][0]
 
 invalidIDsP1 = []
+invalidIDsP2 = []
 for r in ranges:
     for i in range(r[0],r[1]+1):
         n = str(i)
         if(n[:len(n)//2] == n[len(n)//2:]):
             invalidIDsP1.append(i)
-
-print(f"Part 1: {sum(invalidIDsP1)}")
-
-invalidIDsP2 = []
-for r in ranges:
-    for i in range(r[0],r[1]+1):
-        n = str(i) 
         for _,x in enumerate(range(len(n) // 2)):
             if(n.count(n[:x+1]) * (x + 1)  == len(n)):
                 invalidIDsP2.append(i)
                 break
-  
+
+print(f"Part 1: {sum(invalidIDsP1)}") 
 print(f"Part 2: {sum(invalidIDsP2)}")
 
 #Zeit Ende
