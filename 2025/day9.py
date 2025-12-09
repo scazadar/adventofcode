9f5b5cf4edc881438448b831de36dafaa21e086a
scazadar <mb.gielen@gmail.com>
2025-12-09 22:48:19 +0100

    day9

--- 2025/day9.py
+++ 2025/day9.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python3
 #Imports
-import time, math
+import time
+from shapely.geometry import Polygon
+from shapely import box
+import matplotlib.pyplot as plt
 
 #Zeit Start
 start = time.time()
@@ -15,34 +18,30 @@ for i,tile1 in enumerate(grid):
         y = abs(tile1[1] - tile2[1])+1
         retangles.append((x,y))
         
-print(f"Part 1: {max([_[0]*_[1] for _ in retangles])}")
+print(f"Part 1: {max([_[0]*_[1] for _ in retangles])}")        
 
-""" allPointsOnLine = []
-for i,tile in enumerate(grid):
-    if(i+1 < len(grid)):
-        nextTile = grid[i+1] 
-    else:
-        nextTile = grid[0]
-    
-    if(tile[0] == nextTile[0]):
-        tiles = []
-        if(tile[1] < nextTile[1]):
-            tiles = [(tile[0],_) for _ in range(tile[1],nextTile[1])]
-        else:
-            tiles = [(tile[0],_) for _ in range(tile[1],nextTile[1]-1,-1)]
-        allPointsOnLine.extend(tiles)
-    elif(tile[1] == nextTile[1]):
-        tiles = []
-        if(tile[0] < nextTile[0]):
-            tiles = [(_, tile[1]) for _ in range(tile[0],nextTile[0])]
-        else:
-            tiles = [(_, tile[1]) for _ in range(tile[0],nextTile[0]-1,-1)]
-        allPointsOnLine.extend(tiles) """
-        
-    
+poly = Polygon(grid)
+
+""" fig, ax = plt.subplots()
+x, y = poly.exterior.xy
+ax.plot(x, y, color="black")
+ax.grid(True, linestyle=":", linewidth=0.5)
+ax.set_aspect("equal")
+ax.invert_yaxis()   
+plt.show()  """
+
 retangles = []
-for i,tile in enumerate(grid[2:]):
-    pass
+for i,tile1 in enumerate(grid):
+    for tile2 in grid[i+1:]:
+        x = abs(tile1[0] - tile2[0])+1
+        y = abs(tile1[1] - tile2[1])+1
+        rect = box(*tile1,*tile2) 
+
+        if(poly.contains(rect)):
+            retangles.append((x,y)) 
+
+print(f"Part 2: {max([_[0]*_[1] for _ in retangles])}")        
+    
 
 #Zeit Ende
 ende = time.time()
