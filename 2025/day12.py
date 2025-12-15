4495d5f52c31acb56161efdba81dba8dc7ab9399
scazadar <mb.gielen@gmail.com>
2025-12-15 11:28:34 +0100

    day12

--- 2025/day12.py
+++ 2025/day12.py
@@ -11,7 +11,7 @@ start = time.time()
 #Part 1
 input = [r for r in open("inputs/day12").read().split("\n\n")]
 presentShapes = [tuple(tuple(1 if _ == '#' else 0 for _ in _ ) for _ in _[2:].strip().split("\n")) for _ in input[:-1]]
-sizes = {tuple(map(int,_.split(":")[0].split("x"))):tuple(map(int,_.split(":")[1].split())) for _ in input[-1].split("\n")}
+sizes = [tuple([tuple(map(int,_.split(":")[0].split("x"))),tuple(map(int,_.split(":")[1].split()))]) for _ in input[-1].split("\n")]
 
 @cache
 def countParts(presentShape):
@@ -22,14 +22,18 @@ def calcSize(grid, presents):
     all = sum([c*countParts(presentShapes[i]) for i,c in enumerate(presents)])
     return n > all
     
-reducedPresentShapes = {}
+reducedPresentShapes = []
 
 for grid in sizes:
-    if(calcSize(grid,sizes[grid])):
-        reducedPresentShapes.setdefault(grid,sizes[grid])
+    if(calcSize(grid[0],grid[1])):
+        reducedPresentShapes.append(tuple([grid[0],grid[1]]))
         
 for k in reducedPresentShapes:
-    print(k,reducedPresentShapes[k])
+    n = k[0][0] * k[0][1]
+    all = sum([c*countParts(presentShapes[i]) for i,c in enumerate(k[1])])
+    print(k, n, all, n-all)
+    
+print(len(reducedPresentShapes))
 
 
    
